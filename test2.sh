6aaae74ab46016f3be28cc0137532daf69135f8e
zongzixiaojie <47157184+zongzixiaojie@users.noreply.github.com>
2019-12-16 19:58:07 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#../start.sh
 source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Que2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Que2/output
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /Que2/input/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Que2/output/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../framingham.csv /Que2/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077  ./Que2.py hdfs://$SPARK_MASTER:9000/Que2/input/
 #/usr/local/hadoop/bin/hdfs dfs -cat /Que2/output/*
