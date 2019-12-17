6ad42fac029873f26ee4cd20c85bbbbb734be495
zongzixiaojie <47157184+zongzixiaojie@users.noreply.github.com>
2019-12-16 19:38:18 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ../../env.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Que2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Que2/output
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Que2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../framingham.csv /Que2/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077  ./Que2.py hdfs://$SPARK_MASTER:9000/Que2/input/
+#/usr/local/hadoop/bin/hdfs dfs -cat /lab/output/*
